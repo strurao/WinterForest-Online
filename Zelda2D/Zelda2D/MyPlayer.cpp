2a64a4a0a28b7f9c29c028fc19d7fe5a08789b37
strurao <strurao@gmail.com>
2023-08-18 21:27:16 +0900

    Feat: MyPlayer

---
+++ Zelda2D/Zelda2D/MyPlayer.cpp
@@ -0,0 +1,154 @@
+#include "pch.h"
+#include "MyPlayer.h"
+#include "InputManager.h"
+#include "TimeManager.h"
+#include "ResourceManager.h"
+#include "Flipbook.h"
+#include "CameraComponent.h"
+#include "SceneManager.h"
+#include "DevScene.h"
+#include "Arrow.h"
+#include "HitEffect.h"
+
+MyPlayer::MyPlayer()
+{
+	CameraComponent* camera = new CameraComponent();
+	AddComponent(camera);
+}
+
+MyPlayer::~MyPlayer()
+{
+
+}
+
+void MyPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void MyPlayer::Tick()
+{
+	Super::Tick();
+
+	SyncToServer();
+}
+
+void MyPlayer::Render(HDC hdc)
+{
+	Super::Render(hdc);
+}
+
+void MyPlayer::TickInput()
+{
+	_keyPressed = true;
+
+	if (GET_SINGLE(InputManager)->GetButton(KeyType::W))
+	{
+		SetDir(DIR_UP);
+	}
+	else  if (GET_SINGLE(InputManager)->GetButton(KeyType::S))
+	{
+		SetDir(DIR_DOWN);
+	}
+	else if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
+	{
+		SetDir(DIR_LEFT);
+	}
+	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
+	{
+		SetDir(DIR_RIGHT);
+	}
+	else
+	{
+		_keyPressed = false;
+	}
+
+	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::KEY_1))
+	{
+		SetWeaponType(WeaponType::Sword);
+	}
+	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::KEY_2))
+	{
+		SetWeaponType(WeaponType::Bow);
+	}
+	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::KEY_3))
+	{
+		SetWeaponType(WeaponType::Staff);
+	}
+
+	if (GET_SINGLE(InputManager)->GetButton(KeyType::SpaceBar))
+	{
+		SetState(SKILL);
+	}
+}
+
+void MyPlayer::TryMove()
+{
+	if (_keyPressed == false)
+		return;
+
+	Vec2Int deltaXY[4] = { {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
+
+	if (info.dir() == DIR_UP)
+	{
+		Vec2Int nextPos = GetCellPos() + deltaXY[info.dir()];
+		if (CanGo(nextPos))
+		{
+			SetCellPos(nextPos);
+			SetState(MOVE);
+		}
+	}
+	else  if (info.dir() == DIR_DOWN)
+	{
+		Vec2Int nextPos = GetCellPos() + deltaXY[info.dir()];
+		if (CanGo(nextPos))
+		{
+			SetCellPos(nextPos);
+			SetState(MOVE);
+		}
+	}
+	else if (info.dir() == DIR_LEFT)
+	{
+		Vec2Int nextPos = GetCellPos() + deltaXY[info.dir()];
+		if (CanGo(nextPos))
+		{
+			SetCellPos(nextPos);
+			SetState(MOVE);
+		}
+	}
+	else if (info.dir() == DIR_RIGHT)
+	{
+		Vec2Int nextPos = GetCellPos() + deltaXY[info.dir()];
+		if (CanGo(nextPos))
+		{
+			SetCellPos(nextPos);
+			SetState(MOVE);
+		}
+	}
+}
+
+void MyPlayer::TickIdle()
+{
+	TickInput();
+	TryMove();
+}
+
+void MyPlayer::TickMove()
+{
+	Super::TickMove();
+}
+
+void MyPlayer::TickSkill()
+{
+	Super::TickSkill();
+}
+
+void MyPlayer::SyncToServer()
+{
+	if (_dirtyFlag == false)
+		return;
+
+	SendBufferRef sendBuffer = ClientPacketHandler::Make_C_Move();
+	GET_SINGLE(NetworkManager)->SendPacket(sendBuffer);
+}
